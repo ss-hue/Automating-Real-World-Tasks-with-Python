cdb7ad65106d300c37e8d0e1d141ecf8752564d6
ss_roldan <66038679+ss-hue@users.noreply.github.com>
2021-06-07 03:28:09 +0100

    Add week two Graded Assessment Code

---
+++ week_two_graded_ex.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+
+
+path = '/data/feedback/'
+
+docs = os.listdir(path)
+
+dct = {}
+
+def parse_files():
+
+  for d in docs:
+
+    try:
+      with open(path + d, 'r', encoding='utf8') as file:
+              
+        text = file.readlines()
+
+        dct['title'] = text[0].rstrip('\n')
+        dct['name'] = text[1].rstrip('\n')
+        dct['date'] = text[2].rstrip('\n')
+        dct['feedback'] = ''.join(text[3:]).rstrip()
+
+        response = requests.post('http://<corpweb-external-IP>/feedback/', json=dct)
+        print(response.status_code) 
+    except:
+      print('There was an error while opening the file!')
+
+  
+
+ 
+parse_files()
